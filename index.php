25f2c2ea80ba31f1f997cdc957f1ec0c38cf5064
Veikka Kähkönen <veikka.kahkonen@aalto.fi>
2014-09-06 23:28:27 +0300

    Yleistä hienosäätöä Navi korjattu, responsiivisuus korjattu, napit lisätty, siältö päivitetty, yleistä ulkoasun hienosäätöä.

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<?php
-if($_GET['salaisuus'] != 666) {
-  header("Location: /", 302);
-} else {
-  // Ääkköset nodetalle
-  header("Content-type: text/html; charset=utf-8");
-}
-?>
 <!DOCTYPE html>
 <html lang="fi">
   <head>
@@ -30,22 +22,15 @@ if($_GET['salaisuus'] != 666) {
   <script>
     $(document).ready(function() {
       $('#nav').onePageNav({
-              scrollOffset: 512,
+              offset: 500,
               currentClass: 'active',
               changeHash: false,
-              filter: ':not(.external)',
-              begin: function() {
-                //Hack so you can click other menu items after the initial click
-                $('body').append('<div id="device-dummy" style="height: 1px;"></div>');
-              },
-              end: function() {
-                $('#device-dummy').remove();
-              }
-          });
+              filter: ':not(.external)'
+      });
     });
     </script>
    	<div class="container-fluid nohpadding">
-      <div class="navi navbar-fixed-top" role="navigation">
+      <nav class="navi navbar-default navbar-fixed-top navbar" role="navigation">
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -53,62 +38,85 @@ if($_GET['salaisuus'] != 666) {
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand text-uppercase" href="#">Speksi 25v</a>
+            <a class="navbar-brand text-uppercase" href="#"><img src="img/nappi.png" class="img-responsive pull-left nappi">Teekkarispeksi<br>25 vuotta</a>
           </div>
           <div class="navbar-collapse collapse">
             <ul id="nav" class="nav navbar-nav navbar-right text-uppercase">
-              <li><a href="#juhla">Vuosijuhla</a></li>
-              <li><a href="#naytelma">Näytelmä</a></li>
-              <li><a href="#">Liput</a></li>
+              <li><a href="#sect-naytelma">Näytelmä</a></li>
+              <li><a href="#sect-juhla">Vuosijuhla</a></li>
+              <li><a href="http://kauppa.speksi25.com/" class="external">Liput</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
-      </div>
+      </nav>
     </div>
   	<div class="container-fluid nohpadding">
       <div class="row nomargin image-bar-container">
-        <img class="image-bar" src="img/top.jpg" />
+        <img id="img-top" class="image-bar" src="img/top.jpg" />
         <div id="juhlaviikko" class="row nomargin text-center text-uppercase">Speksin 25-vuotisjuhlaviikko</div>
+        <div id="sect-naytelma" class="section"></div>
       </div>
-  		<div id="naytelma" class="row row-centered nomargin">
-        <div class="col-xs-1 col-centered"></div>
-        <div class="col-xs-8 col-centered">
+  		<div id="naytelma" class="row row-centered nomargin content-bar">
+        <div class="col-lg-1 col-centered"></div>
+        <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
           <h2>25-VUOTISJUHLAREVYY 13.-14.10.2014</h2>
           <p>
-          Teekkarispeksin 25-vuotisjuhlarevyy on koko illan teatterispektaakkeli, joka tarjoaa vain kahdessa näytöksessä 13.-14.10.2014 mahdollisuuden nähdä Aleksanterin teatterin lavalla läpileikkauksen Teekkarispeksin neljännesvuosisadan historiaan. Yhteensä yli 60 näyttelijää ja tanssijaa aina 1990-luvun hämäristä viime vuosiin saakka palaavat parrasvaloihin vielä kerran viihdyttämään juuri sinua.
-          </p>
-          <p>
-          Menneiden vuosien speksihahmot nähdään niin klassisissa kohtauksissa kuin myös uusissa ja odottamattomissakin tilanteissa, orkesteri koostuu menneiden speksibändien legendaarisista jäsenistä, musiikki on speksibiisien parhaimmistoa – ja kokonaisuuden kruunaavat luonnollisesti yleisön omstart-huudot sekä niiden aiheuttamat ainutlaatuiset käänteet.
-          </p>
-          <p>
+          Teekkarispeksin 25-vuotisjuhlarevyy on koko illan teatterispektaakkeli, joka tarjoaa kahdessa näytöksessä 13.-14.10.2014 mahdollisuuden nähdä Aleksanterin teatterin lavalla läpileikkauksen Teekkarispeksin neljännesvuosisadan historiaan. Yhteensä yli 60 näyttelijää ja tanssijaa aina 1990-luvun hämäristä viime vuosiin saakka palaavat parrasvaloihin vielä kerran viihdyttämään juuri sinua.
+        </p>
+        <p>
+          <b>Näytösajat:</b> 13.-14.10.2014 kello 19.00<br>
+          <b>Paikka:</b> Aleksanterin teatteri, Albertinkatu 32, Helsinki<br>
+          <b>Liput:</b> 20/25 euroa (opiskelijat 15 euroa)
+        </p>
+        <p>
+          <a href="http://kauppa.speksi25.com/" class="btn btn-default btn-lg" role="button">Osta lippuja</a>
+          <a href="https://www.facebook.com/events/640057099442093" class="btn btn-default btn-lg btn-fb" role="button">Facebook-tapahtuma</a><br>
+        </p>
+        <p>
+          Juhlarevyyssä menneiden vuosien speksihahmot nähdään niin klassisissa kohtauksissa kuin myös uusissa ja odottamattomissakin tilanteissa, orkesteri koostuu menneiden speksibändien legendaarisista jäsenistä, musiikki on speksibiisien parhaimmistoa - ja kokonaisuuden kruunaavat luonnollisesti yleisön omstart-huudot sekä niiden aiheuttamat ainutlaatuiset käänteet.
+        </p>
+        <p>
           Esitys ei kuitenkaan ole vain ilotulitusta aisteille, vaan ohjelmanumerot nivoo yhteen tarina speksiläisyydestä. Mitä tarkoittaa, että speksissä ovat komeimmat miehet ja kauneimmat naiset? Että speksi on älyn voitto materiasta ja luovuuden voitto rutiineista? Että speksi on ja voi?
-          </p>
-          <p>
-          Tule ottamaan selvää – ja näkemään samalla parhaat palat 25 vuoden teekkarispeksihistoriasta!
-          </p>
+        </p>
+        <p>
+          <b>Tule ottamaan selvää - ja näkemään parhaat palat 25 vuoden teekkarispeksihistoriasta!</b>
+        </p>
         </div>
     </div>
     <div class="row nomargin image-bar-container">
         <img class="image-bar" src="img/mid.jpg" />
+        <div id="sect-juhla" class="section"></div>
     </div>
-      <div id="juhla" class="row row-centered nomargin">
-        <div class="col-xs-1 col-centered"></div>
-        <div class="col-xs-8 col-centered">
-          <h2>Teekkarispeksin 25-vuotisjuhla 18.10.2014</h2>
+      <div id="juhla" class="row row-centered nomargin content-bar">
+        <div class="col-lg-1 col-centered"></div>
+        <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
+          <h2>Teekkarispeksin 25-vuotisjuhlagaala 18.10.2014</h2>
           <p>
-          Teekkarispeksin 25. juhlavuosi huipentuu lauantaina 18.10.2014 juhlagaalaan, johon ovat lämpimästi tervetulleita kaikki Teekkarispeksin tekemiseen neljännesvuosisadan aikana osallistuneet vanhat ja uudet speksiläiset seuralaisineen.
-          </p>
-          <p>
-Kyseessä on speksihenkinen iltajuhla, josta kaikenlainen jäykistely on kaukana: mukana on ruokaa, juomaa sekä esityksiä - ja toivottavasti myös juuri sinä! Pukukoodi on juhlava mutta vapaamuotoinen, juhlapaikka salaisuuden verhon kätköissä mutta huhujen mukaan Helsingin keskusta-alueen liepeillä.
-          </p>
+          Teekkarispeksin 25. juhlavuosi huipentuu lauantaina 18.10.2014 juhlagaalaan, johon ovat lämpimästi tervetulleita kaikki Teekkarispeksin tekemiseen neljännesvuosisadan aikana osallistuneet vanhat ja uudet speksiläiset seuralaisineen. Luonnollisesti myös aikaisempien vuosikymmenten teekkaritempausten speksiläiset ovat tervetulleita mukaan juhliin, sillä nykymuotoinen Teekkarispeksi edistää jo 1930-luvulla alkanutta teekkareiden speksiperinnettä.
+        </p>
+        <p>
+          Kyseessä on speksihenkinen iltajuhla, josta kaikenlainen jäykistely on kaukana: mukana on ruokaa, juomaa sekä esityksiä pikkutunneille saakka. Pukukoodi on juhlava mutta vapaamuotoinen. Juhlapaikan hulppeat puitteet tarjoaa Suvilahden Kattilahalli Helsingissä.
+        </p>
+        <p>
+          Illan aikana halukkailla on mahdollisuus saunoa, joten otathan pyyhkeen mukaan.
+        </p>
+        <p>
+          <b>Aika:</b> Lauantai 18.10.2014 kello 19 <span class="glyphicon glyphicon-arrow-right glyph-nuoli"></span><br>
+          <b>Paikka:</b> Suvilahden Kattilahalli, Sörnäisten rantatie 22, Helsinki<br>
+          <b>Illalliskortit:</b> 50/85 euroa
+        </p>
+        <p>
+          <a href="http://kauppa.speksi25.com/" class="btn btn-default btn-lg" role="button">Ilmoittaudu</a>
+          <a href="https://www.facebook.com/events/679685888786138" class="btn btn-default btn-lg btn-fb" role="button">Facebook-tapahtuma</a><br>   
+        </p>
         </div>
       </div>
-      <div id="bw" class="row row-centered nomargin">
-        <div class="col-xs-1 col-centered"></div>
-        <div class="col-xs-8 col-centered">
+      <div id="bw" class="row row-centered nomargin content-bar">
+        <div class="col-lg-1 col-centered"></div>
+        <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
           <h2>Yleistä tietoa</h2>
           <p>
-            Put a bird on it flannel mlkshk before they sold out tattooed, Banksy Blue Bottle Wes Anderson. Helvetica Pinterest hella brunch. Forage slow-carb organic, biodiesel typewriter cred Brooklyn butcher deep v sartorial authentic. Viral post-ironic Wes Anderson pop-up. Bushwick tousled Tonx Thundercats, XOXO drinking vinegar lo-fi sustainable actually cliche flannel squid. Ethnic brunch church-key Pinterest. Ethnic readymade 8-bit XOXO pork belly, single- origin coffee fingerstache farm-to-table jean shorts Banksy iPhone paleo vinyl quinoa sustainable.
+            Put a bird on it flannel mlkshk before they sold out tattooed, Banksy Blue Bottle Wes Anderson. Helvetica Pinterest hella brunch. Forage slow-carb rganic, biodiesel typewriter cred Brooklyn butcher deep v sartorial authentic. Viral post-ironic Wes Anderson pop-up. Bushwick tousled Tonx Thundercats, XOXO drinking vinegar lo-fi sustainable actually cliche flannel squid. Ethnic brunch church-key Pinterest. Ethnic readymade 8-bit XOXO pork belly, single- origin coffee fingerstache farm-to-table jean shorts Banksy iPhone paleo vinyl quinoa sustainable.
           </p>
         </div>
       </div>
