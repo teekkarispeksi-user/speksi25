2d96c37e661248565a5f43f7c22499c0a11b4ddf
Teppo Nurminen <teppo@rakettitiede.com>
2014-09-07 17:04:41 +0300

    some content, more images

--- index.php
+++ index.php
@@ -42,9 +42,9 @@
           </div>
           <div class="navbar-collapse collapse">
             <ul id="nav" class="nav navbar-nav navbar-right text-uppercase">
-              <li><a href="#sect-naytelma">Näytelmä</a></li>
+              <li><a href="#sect-naytelma">Revyy</a></li>
               <li><a href="#sect-juhla">Vuosijuhla</a></li>
-              <li><a href="http://kauppa.speksi25.com/" class="external">Liput</a></li>
+              <li><a href="kauppa/" class="external">Liput</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
@@ -54,12 +54,32 @@
       <div class="row nomargin image-bar-container">
         <img id="img-top" class="image-bar" src="img/top.jpg" />
         <div id="juhlaviikko" class="row nomargin text-center text-uppercase">Speksin 25-vuotisjuhlaviikko</div>
-        <div id="sect-naytelma" class="section"></div>
+        <div id="sect-ohjelma" class="section"></div>
       </div>
-  		<div id="naytelma" class="row row-centered nomargin content-bar">
+      <div id="ohjelma" class="row row-centered nomargin content-bar valkoinen">
         <div class="col-lg-1 col-centered"></div>
         <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
-          <h2>25-VUOTISJUHLAREVYY 13.-14.10.2014</h2>
+          <h2>Vuosijuhlaviikon tapahtumat</h2>
+          <p>
+            Juhlaviikon ohjelma tarjoaa nähtävää ja koettavaa sekä uusille että erityisesti vanhoille speksiläisille.
+          </p>
+          <h3>Ohjelma</h3>
+          <ul>
+            <li><a href="#sect-naytelma">Juhlarevyy Aleksanterin teatterissa ma 13.10.2014</a></li>
+            <li><a href="#sect-naytelma">Juhlarevyy Aleksanterin teatterissa ti 14.10.2014</a></li>
+            <li><a href="http://teekkarispeksi.fi/speksi/tapahtumat/vanhan-speksin-katseluilta-gorsussa" class="external">Vanhojen speksien katseluilta to 16.10.2014</a></li>
+            <li><a href="#sect-juhla">Vuosijuhlagaala la 18.10.2014</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="row nomargin image-bar-container">
+        <img class="image-bar" src="img/3.jpg" />
+        <div id="sect-naytelma" class="section"></div>
+      </div>
+  		<div id="naytelma" class="row row-centered nomargin content-bar keltainen">
+        <div class="col-lg-1 col-centered"></div>
+        <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
+          <h2>25-vuotisjuhlarevyy 13.-14.10.2014</h2>
           <p>
           Teekkarispeksin 25-vuotisjuhlarevyy on koko illan teatterispektaakkeli, joka tarjoaa kahdessa näytöksessä 13.-14.10.2014 mahdollisuuden nähdä Aleksanterin teatterin lavalla läpileikkauksen Teekkarispeksin neljännesvuosisadan historiaan. Yhteensä yli 60 näyttelijää ja tanssijaa aina 1990-luvun hämäristä viime vuosiin saakka palaavat parrasvaloihin vielä kerran viihdyttämään juuri sinua.
         </p>
@@ -69,7 +89,7 @@
           <b>Liput:</b> 20/25 euroa (opiskelijat 15 euroa)
         </p>
         <p>
-          <a href="http://kauppa.speksi25.com/" class="btn btn-default btn-lg" role="button">Osta lippuja</a>
+          <a href="kauppa/" class="btn btn-default btn-lg" role="button">Osta lippuja</a>
           <a href="https://www.facebook.com/events/640057099442093" class="btn btn-default btn-lg btn-fb" role="button">Facebook-tapahtuma</a><br>
         </p>
         <p>
@@ -84,13 +104,13 @@
         </div>
     </div>
     <div class="row nomargin image-bar-container">
-        <img class="image-bar" src="img/mid.jpg" />
+        <img class="image-bar" src="img/5.jpg" />
         <div id="sect-juhla" class="section"></div>
     </div>
-      <div id="juhla" class="row row-centered nomargin content-bar">
+      <div id="juhla" class="row row-centered nomargin content-bar mustavalkoinen">
         <div class="col-lg-1 col-centered"></div>
         <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
-          <h2>Teekkarispeksin 25-vuotisjuhlagaala 18.10.2014</h2>
+          <h2>Vuosijuhlagaala 18.10.2014</h2>
           <p>
           Teekkarispeksin 25. juhlavuosi huipentuu lauantaina 18.10.2014 juhlagaalaan, johon ovat lämpimästi tervetulleita kaikki Teekkarispeksin tekemiseen neljännesvuosisadan aikana osallistuneet vanhat ja uudet speksiläiset seuralaisineen. Luonnollisesti myös aikaisempien vuosikymmenten teekkaritempausten speksiläiset ovat tervetulleita mukaan juhliin, sillä nykymuotoinen Teekkarispeksi edistää jo 1930-luvulla alkanutta teekkareiden speksiperinnettä.
         </p>
@@ -109,17 +129,45 @@
           <a href="http://kauppa.speksi25.com/" class="btn btn-default btn-lg" role="button">Ilmoittaudu</a>
           <a href="https://www.facebook.com/events/679685888786138" class="btn btn-default btn-lg btn-fb" role="button">Facebook-tapahtuma</a><br>   
         </p>
+        <h3>VIP-vieraaksi?</h3>
+        <p>
+          Tunnetko itsesi tärkeämmäksi kuin muut? Haluatko jakaa menestystäsi meille muille? Haluatko omaa tilaa ja erinomaisempaa palvelua?
+        </p>
+        <p>
+        Vuosijuhlagaalaan on myynnissä myös rajoitettu erä VIP-paketteja, jotka antavat erinomaisen tavan tarjota tukeaan Teekkarispeksi ry:lle. VIP-vierailla on oma sisäänkäynti ohi jonojen, erillinen VIP-lounge tarjoiluineen, mahdollisuus päästä backstagelle tapaamaan illan esiintyjiä, sekä paljon muuta!
+        </p>
+        <p>
+          Lisätietoja <a href="kauppa/">lippukaupasta.</a>
+        </p>
         </div>
       </div>
-      <div id="bw" class="row row-centered nomargin content-bar">
+      <div class="row nomargin image-bar-container">
+        <img class="image-bar" src="img/1.jpg" />
+        <div id="sect-mikaspeksi" class="section"></div>
+      </div>
+      <div id="mikaspeksi" class="row row-centered nomargin content-bar valkoinen">
         <div class="col-lg-1 col-centered"></div>
         <div class="col-xs-12 col-sm-9 col-lg-8 col-centered">
-          <h2>Yleistä tietoa</h2>
+          <h2>Teekkarispeksin historia</h2>
+          <h3>Speksi Ruotsissa</h3>
           <p>
-            Put a bird on it flannel mlkshk before they sold out tattooed, Banksy Blue Bottle Wes Anderson. Helvetica Pinterest hella brunch. Forage slow-carb rganic, biodiesel typewriter cred Brooklyn butcher deep v sartorial authentic. Viral post-ironic Wes Anderson pop-up. Bushwick tousled Tonx Thundercats, XOXO drinking vinegar lo-fi sustainable actually cliche flannel squid. Ethnic brunch church-key Pinterest. Ethnic readymade 8-bit XOXO pork belly, single- origin coffee fingerstache farm-to-table jean shorts Banksy iPhone paleo vinyl quinoa sustainable.
+            Speksiperinteen juuret ulottuvat Pohjanlahden toiselle puolelle, 1500-luvun Uppsalaan, jossa ylioppilaille opetettiin latinaa teatterin avulla. Näytelmät perustuivat klassisiin teemoihin ja silloisesta opiskelijoiden sukupuolijakaumasta johtuen näyttelijät olivat kaikki miehiä. He esittivät niin historian suurmiehiä kuin hemaisevia kaunottariakin. Seuraavien 300 vuoden kuluessa ruotsalainen speksi sai nykyisen muotonsa improvisaatioon nojaavana musikaalifarssina. Tekniikan ylioppilaat ovat olleet vahvasti mukana luomassa ja muovaamassa speksiperinnettä ja Ruotsin suurin speksi onkin pitkään ollut Kungliga Teknikska Högskolanin Kårspexet. Speksit elävät naapurimaassamme voimakkaana ja nykyään lähes jokaisella opiskelijayhteisöllä on oma vuotuinen speksinsä.
           </p>
+          <h3>Speksin ensiaskeleet Suomessa</h3>
+          <p>
+            Suomeen speksi rantautui vuonna 1933 Teknillisen Korkeakoulun Ylioppilaskunnan menestyksekkäänä tempauksena. Tempausmuotoisia teekkarispeksejä toteutettin parinkymmenen vuoden tauon jälkeen vielä viisi kappaletta 1950- ja 1960-luvuilla. Teekkarispeksi saavutti suunnattoman suosion niin teekkareiden kuin suurenkin yleisön keskuudessa. Näiden jo legendaarisen maineen saavuttaneiden produktioiden jälkeen koitti kuitenkin pitkä hiljaisuus. Aatteen palo sammutti speksiperinteen 60-luvulla ja viimeinen vanhan ajan Teekkarispeksi Vanhat portaat esitettiin vuonna 1967. Speksiperinne hiipui ja unohtui. Ainoastaan 1950-luvulla alkanut ruotsinkielinen Medicinarklubben Thoraxin speksi porskutti eteenpäin ja onkin nykyään kiistatta vanhin suomalainen speksi.
+          </p>
+          <h3>Speksin toinen tuleminen</h3>
+          <p>
+            Speksiperinne alkoi heräillä henkiin, kun Lääketieteellisen kandidaattiseuran ensimmäinen speksi sai ensi-iltansa vuonna 1988, vieden toisiksi vanhimman suomalaisen speksin arvonimen. Kului vielä kaksi vuotta ennen kuin tekniikan ylioppilaista löytyi jälleen se tarvittava kipinä, jonka speksin tekeminen vaatii. Kaksi kunnianhimoista teekkaria päätti herättää speksiperinteen henkiin ja näin syntyi Gordon. Se oli menestys: kahdessa esityksessä kävi lähes tuhat katsojaa. Suomen kolmanneksi vanhin speksi oli saanut alkunsa. Kun pyörä oli saatu pyörimään, ei sitä voinut pysäyttää enää mikään. Toinen toistaan upeammat Teekkarispeksit seurasivat Gordonia. Ammattiohjaaja tuotiin mukaan jo alkuvaiheessa ja onkin pysynyt Teekkarispeksin omaleimaisena piirteenä: jopa Ruotsissa ohjaajat valitaan opiskelijoiden joukosta. Vuosien saatossa Teekkarispeksin ammattitaito ja kunnianhimo on kasvanut: esimerkiksi teatteritekniikan toteutus lähentelee nykyään ammattilaisjälkeä. Teekkarispeksi on vakiinnuttanut asemansa teekkareiden ja muiden opiskelijoiden keskuudessa ja löytänyt omat ystävänsä myös suuresta yleisöstä.
+          </p>
+          <p><strong>Lisää tietoa Teekkarispeksistä löydät yhdistyksen sivuilta: <a href="http://www.teekkarispeksi.fi/">www.teekkarispeksi.fi.</a></strong>
         </div>
       </div>
-	</div>
+      <div class="row nomargin image-bar-container">
+        <img class="image-bar" src="img/2.jpg" />
+        <div id="sect-juhla" class="section"></div>
+      </div>
+	  </div>
 	</body>
 </html>
